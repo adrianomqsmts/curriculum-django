286260b90356a5f3c4b8af640fd1da4098882489
Adrianomqsmts <adriano_mqsmts@outlook.com.br>
2020-12-10 02:05:13 -0300

    abstract tamplates

--- curriculo/settings.py
+++ curriculo/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import dj_database_url
+
 from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,3 +125,4 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGOUT_REDIRECT_URL = 'index'
